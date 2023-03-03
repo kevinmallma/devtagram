a930172d501792e525e6e80128a23f50c8ac245c
Kevin Mallma Camargo <kevinmallma98@gmail.com>
2023-03-03 17:42:54 -0500

    Comienzo del sisitema laravel

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Crea una nueva Publicación
+@endsection
+
+@push('styles')
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
+@endpush
+
+@push('js')
+    @vite('resources/js/bootstrap.js')
+@endpush
+
+@section('contenido')
+    <div class="md:flex md:items-center">
+        <div class="md:w-1/2 px-10">
+            <form id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-column justify-center items-center" action="{{ route('imagenes.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+            </form>
+        </div> 
+        <div class="md:w-1/2 p-10 bg-white rounded-lg shadow-xl mt-10 md:mt-0">
+            <form method="POST" action="{{ route('posts.store') }}" novalidate>
+                @csrf
+                <div class="mb-5">
+                    <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Titulo
+                    </label>
+                    <input type="text" id="titulo" name="titulo" placeholder="Titulo de la publicación" class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror" value="{{ old('titulo') }}" />
+                    @error('titulo')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea id="descripcion" name="descripcion" placeholder="Descripción de la publicación" class="border p-3 w-full rounded-lg @error('descripcion') border-red-500 @enderror">{{ old('descripcion') }}</textarea>
+                    @error('descripcion')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <input type="hidden" name="imagen" value="{{ old('imagen') }}" />
+                    @error('imagen')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+                <input type="submit" value="Crear publicación" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg" />
+            </form>
+        </div>
+    </div>
+@endsection
